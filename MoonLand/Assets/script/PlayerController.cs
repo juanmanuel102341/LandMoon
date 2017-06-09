277c93d1a44fdd5fabb7f2e47e57d076537e3a67
juanmanuel102341 <jms24@live.com.ar>
2017-06-09 09:24:24 -0300

    setiando ignore list y agregue assets

---
+++ MoonLand/Assets/script/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float velocity=50;
+	private float time;
+	// Use this for initialization
+	private Rigidbody2D rb2d;
+	private float impulso=0;
+	void Awake(){
+		rb2d=GetComponent<Rigidbody2D>();
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	time+=Time.deltaTime;
+
+	
+			print("upppp");
+		impulso=Input.GetAxis("vertical")*velocity;
+
+			
+
+		impulso*=Time.deltaTime;
+
+		print("flotante "+impulso);
+				
+
+		rb2d.transform.Translate(0,impulso,0);
+	
+
+
+	}
+}
